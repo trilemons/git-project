9c90100002b29ae3a29a7c00f263d976fc096480
lemon <2065524097l@gmail.com>
2024-07-11 18:13:41 +0800

    master forth commit

--- src/main/java/com/lemon/gitTest/GitTest.java
+++ src/main/java/com/lemon/gitTest/GitTest.java
@@ -4,9 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!2");
-        System.out.println("hello git!3");
-        System.out.println("hello git!4");
-        System.out.println("master test!!");
-        System.out.println("side test!!");
+
     }
 }
